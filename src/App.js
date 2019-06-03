a7a5c4f4abd5f11c66f73c76b506f78ae05a576b
AliLynne <alilynne.thompson@gmail.com>
2019-06-03 09:10:43 -0700

    created & displayed header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App__Header">
-        <h1>Ali Thompson</h1>
-        <p>Contact Me: <a href="mailto:alilynne.thompson@gmail.com">alilynne.thompson@gmail.com</a></p>
-        <a href="https://github.com/AliLynne/">GitHub</a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="App__Header">
+      <h1>Ali Thompson</h1>
+      <p>Contact Me: <a href="mailto:alilynne.thompson@gmail.com">alilynne.thompson@gmail.com</a></p>
+      <a href="https://github.com/AliLynne/">GitHub</a>
+    </header>
+  );
+}
+
+export default Header;
