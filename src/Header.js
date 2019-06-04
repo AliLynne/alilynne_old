a05ff63c37b19539b83ed24123f61ad615029867
AliLynne <alilynne.thompson@gmail.com>
2019-06-04 08:22:36 -0700

    resolves #11 resolves #12

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 function Header() {
   return (
-    <header className="App__Header">
+    <header className="Header">
       <h1>Ali Lynne Thompson</h1>
       <p>Contact Me: <a href="mailto:alilynne.thompson@gmail.com">alilynne.thompson@gmail.com</a></p>
       <a href="https://github.com/AliLynne/">GitHub</a>
+      <ul className="Header__List">
+        <li className="Header__ListItem">
+          <Link to="/blog/">Blog</Link>
+          <Link to="/projects/">Projects</Link>
+        </li>
+      </ul>
     </header>
   );
 }
